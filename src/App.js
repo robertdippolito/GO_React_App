716a939f933b6740994ab39d1f71293499843419
Robert D'Ippolito <robertdippolito@Roberts-MacBook-Pro-2.local>
2018-08-06 23:00:13 -0400

    deleted the commented code

--- src/App.js
+++ src/App.js
@@ -31,30 +31,6 @@ class App extends Component {
     const orderedArray = availableStops.sort();
     console.log('Next 3 Stops Include: ' + orderedArray[0] + ', ' + orderedArray[1] + ', and ' + orderedArray[2]);
 
-    // let convertedDay = moment(calendar[0].service_id, 'YYYYMMDD').format('YYYY-MM-DD');
-    // let currentDay = moment().format('YYYY-MM-DD');
-
-    // var now = moment().format('HH:mm:ss');
-
-    // console.log(tripList.length);
-    // for(var i=0; i<tripList.length; i++) {
-    //   if(tripList[i].stop_id === 'GU') {
-    //     if(tripList[i].stop_headsign.includes('Guildwood') && tripList[i].stop_headsign.includes('Union')){
-    //       guildWoodStops.push(tripList[i].arrival_time);
-    //     }
-    //   }
-    // }
-    //
-    // for(var j=0; j<guildWoodStops.length; j++) {
-    //   if(guildWoodStops[j] > now) {
-    //     availableStops.push(guildWoodStops[j]);
-    //   }
-    // }
-    //
-    // const orderedArray = availableStops.sort();
-    // console.log(orderedArray);
-    // console.log('Next 3 Stops: ' + orderedArray[0] + ' , ' + orderedArray[31] + ' , ' + orderedArray[61]);
-
     return (
       <div><h1>Hello World!</h1></div>
     );
